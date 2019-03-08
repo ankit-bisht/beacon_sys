c4a1a1a8de1c18bd5a1a22d34568b6f2cee03000
suruchi <suruchi.sihra@kelltontech.com>
2019-03-08 23:45:00 +0530

    added migration

---
+++ database/migrations/2019_03_08_174210_beacon_connections_lists.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class BeaconConnectionsLists extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('beacon_connections_lists', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('edge_name',70)->comment = "Edge names";
+            $table->string('first_node',70)->comment = "node 1";
+            $table->string('second_name',70)->comment = "node 2";
+            $table->integer('distance')->comment = "distance between nodes";
+            $table->string('direction_name',70)->comment = "direction names";
+            $table->Integer('created_on');
+            $table->Integer('updated_on');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+
+    }
+}
